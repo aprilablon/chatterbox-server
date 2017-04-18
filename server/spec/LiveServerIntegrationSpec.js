b263cc7d65f023c5144712e85ae213bdf00e5e57
Hack Reactor Student <student@hackreactor.com>
2017-04-18 11:07:27 -0700

    Added two tests for 404 POST and OPTIONS requests

--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -38,7 +38,7 @@ describe('server', function() {
       uri: 'http://127.0.0.1:3000/classes/messages',
       json: {
         username: 'Jono',
-        message: 'Do my bidding!'}
+        text: 'Do my bidding!'}
     };
 
     request(requestParams, function(error, response, body) {
@@ -52,7 +52,7 @@ describe('server', function() {
       uri: 'http://127.0.0.1:3000/classes/messages',
       json: {
         username: 'Jono',
-        message: 'Do my bidding!'}
+        text: 'Do my bidding!'}
     };
 
     request(requestParams, function(error, response, body) {
@@ -60,7 +60,7 @@ describe('server', function() {
       request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
         var messages = JSON.parse(body).results;
         expect(messages[0].username).to.equal('Jono');
-        expect(messages[0].message).to.equal('Do my bidding!');
+        expect(messages[0].text).to.equal('Do my bidding!');
         done();
       });
     });
@@ -73,5 +73,25 @@ describe('server', function() {
     });
   });
 
+  it('should not respond to OPTIONS request with incorrect URL', function(done) {
+    var requestParams = {method: 'OPTIONS',
+      uri: 'http://127.0.0.1:3000/gibberish'
+    };
+
+    request(requestParams, function(error, response, body) {
+      // Now if we request the log, that message we posted should be there:
+      request('http://127.0.0.1:3000/gibberish', function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  it('Should 404 when POST-ing to an incorrect URL', function(done) {
+    request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
 
 });
